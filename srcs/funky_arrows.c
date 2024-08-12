e5881c1af8e56e6a5d6491893258b61813774a2e
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-08-12 14:22:29 +0300

    12.8

--- srcs/funky_arrows.c
+++ srcs/funky_arrows.c
@@ -6,7 +6,7 @@
 /*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 10:33:37 by iniska            #+#    #+#             */
-/*   Updated: 2024/08/09 11:54:00 by iniska           ###   ########.fr       */
+/*   Updated: 2024/08/12 14:12:44 by iniska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,48 @@ static void	close_files(t_bananas *bana)
 	}
 }
 
+static char	*find_path(const char *cmd, char **envp)
+{
+	char	*path_env;
+	char	**paths;
+	char	*full;
+	int		i;
+
+	while (*envp && ft_strncmp(*envp, "PATH=", 5) != 0)
+		envp++;
+	if(!*envp)
+		return (NULL);
+	
+	path_env = *envp + 5;
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		return (NULL);
+
+	i = 0;
+	while(paths[i])
+	{
+		full = ft_strjoin(paths[i], "/");
+		full = ft_strjoin(full, cmd);
+
+		if (access(full, X_OK) == 0)
+		{
+			free_char_array(&paths, i);
+			return (full);
+		}
+		free(full);
+		i++;
+	}
+	free_char_array(&paths, i);
+	return (NULL);
+	 
+
+}
 
 static void	execute_rdr(t_bananas *bana, char **envp)
 {
 	pid_t	pid;
 	int		status;
+	char	*path;
 
 	pid = fork();
 	if (pid < 0)
@@ -48,17 +85,35 @@ static void	execute_rdr(t_bananas *bana, char **envp)
 	{
 		if (bana->infile_count > 0)
 		{
-			if (dup2(bana->in_files[bana->infile_count - 1], STDOUT_FILENO) < 0)
+			if (dup2(bana->in_files[bana->infile_count - 1], STDOUT_FILENO) < 0) // SHOULD THIS -1 BE REMOVED
 			{
-				perror("Bananas! Error in dup2 for output redirection");
+				perror("Bananas! Error in dup2 for output redirection\n");
 				exit(EXIT_FAILURE); //CHECK THIS!
 			}
 		}
+		
+		if(bana->outfile_count > 0)
+		{
+			if (dup2(bana->out_files[bana->outfile_count], STDOUT_FILENO) < 0)
+			{
+				perror("Bananas! Error in dup2 output redirections\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		
 		close_files(bana);
-		ft_printf("%s\n", bana->token[0]);
-		execve(bana->token[0], bana->token, envp);
-		envp = NULL;
+		path = find_path(bana->token[0], envp);
+		if(!path)
+		{
+			perror("Command is bananas:");
+			exit(EXIT_FAILURE);
+		}
 		token_cleaner(bana, 0);
+		ft_printf("			PATH:: %s\n", path);
+		execve(path, bana->token, envp);
+		//envp = NULL;
+		free(path);
+
 		//perror("Bananas! Error in rdr execve");
 	}
 	else
