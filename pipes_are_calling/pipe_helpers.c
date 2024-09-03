b470debde1f6bedf3955a08bf177bbe3c01dc1e6
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-09-03 11:03:29 +0300

    cleaning

---
+++ pipes_are_calling/pipe_helpers.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_helpers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 10:53:11 by iniska            #+#    #+#             */
+/*   Updated: 2024/09/03 10:55:56 by iniska           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+
+void handle_sigint_s(int sig)
+{
+    (void)sig;
+    signal(SIGINT, SIG_DFL);
+    _exit(1);
+}
+
+void	free_argh(char **argh)
+{
+	int i;
+
+	i = 0;
+	while(argh[i])
+	{
+		free(argh[i]);
+		i++;
+	}
+	free(argh);
+}
+
+void	shut_fd(int fd[2])
+{
+	close(fd[0]);
+	close(fd[1]);
+}
