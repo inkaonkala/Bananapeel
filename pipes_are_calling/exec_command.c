0a9cdf72d968c3cdc25fa09d7f676b27a1d7f251
inkaonkala <70053619+inkaonkala@users.noreply.github.com>
2024-09-10 13:50:09 +0300

    funky

--- pipes_are_calling/exec_command.c
+++ pipes_are_calling/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 09:46:16 by iniska            #+#    #+#             */
-/*   Updated: 2024/09/06 11:37:58 by iniska           ###   ########.fr       */
+/*   Updated: 2024/09/10 10:34:12 by iniska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,6 @@ static void	execute_command(t_bananas *bana, char **envp, int index)
 	//if ((ft_strncmp(cmd_args[0],  "cat", 3) == 0)  && cmd_args[1] == NULL || 
 	//	(ft_strncmp(cmd_args[0], "grep", 4) == 0) && cmd_args[2] == NULL)
 	//		empty_prompt();
-
 
 	if(bana->cmd_paths[index])
 	{
