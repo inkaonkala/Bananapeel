e902d30fa29920bd1d2e0db03a09a5eab3b845b0
Joseph Bremser <jbremser@c1r1p1.hive.fi>
2024-09-17 11:33:47 +0300

    token_count check

--- srcs/token/parsing.c
+++ srcs/token/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: jbremser <jbremser@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:32:34 by jbremser          #+#    #+#             */
-/*   Updated: 2024/09/17 09:51:51 by iniska           ###   ########.fr       */
+/*   Updated: 2024/09/17 11:32:55 by jbremser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,6 +135,8 @@ bool	parsing(char *str, t_bananas *bana)
 	int		token_count;
 
 	token_count = count_tokens(str);
+	if (token_count == 0)
+		return (true);
 	free_char_array(bana->envp);
 	bana->envp = list_to_eepie(&bana->env);
 	tokens = ft_calloc((token_count + 1), sizeof(char *));
